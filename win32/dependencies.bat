e3ebf423c613b5c845ce25f48e869835f71f507a
Tapio Vierros <tapiovierros@gmail.com>
2010-04-10 10:18:21 +0300

    Add some Windows build stuff.

---
+++ win32/dependencies.bat
@@ -0,0 +1,17 @@
+REM This batch script aims to help
+REM setting up the dependencies.
+
+REM DOWNLOAD
+
+REM Boost http://www.boost.org/
+wget http://downloads.sourceforge.net/project/boost/boost/1.42.0/boost_1_42_0.7z?use_mirror=switch
+
+REM Box2D http://www.box2d.org/
+wget http://box2d.googlecode.com/files/Box2D_v2.1.0.zip
+
+REM SDL http://www.libsdl.org
+wget http://www.libsdl.org/release/SDL-devel-1.2.14-mingw32.tar.gz
+REM wget http://www.libsdl.org/release/SDL-1.2.14-win32.zip
+
+REM SOIL http://www.lonesock.net/soil.html
+wget http://www.lonesock.net/files/soil.zip
