27085ca1376285177c12dae2418949f67c17986b
Tapio Vierros <tapiovierros@gmail.com>
2010-04-18 22:40:27 +0300

    Some preparation for server.

--- main.cc
+++ main.cc
@@ -21,6 +21,7 @@
 #define scrH 600
 
 
+/// Keyboard input
 bool handle_keys(Players& players) {
 	SDL_Event event;
 	while(SDL_PollEvent(&event)) {
@@ -31,6 +32,7 @@ bool handle_keys(Players& players) {
 			int k = event.key.keysym.sym;
 			if (k == SDLK_ESCAPE) return false;
 			for (Players::iterator it = players.begin(); it != players.end(); ++it) {
+				if (it->type != Actor::HUMAN) continue;
 				if (k == it->KEY_LEFT) it->move(-1);
 				else if (k == it->KEY_RIGHT) it->move(1);
 				if (k == it->KEY_UP) it->jump();
@@ -42,6 +44,7 @@ bool handle_keys(Players& players) {
 		case SDL_KEYUP: {
 			int k = event.key.keysym.sym;
 			for (Players::iterator it = players.begin(); it != players.end(); ++it) {
+				if (it->type != Actor::HUMAN) continue;
 				if (k == it->KEY_UP) it->jumping = 0;
 				//if (k == it->KEY_DOWN) it->duck();
 				if (k == it->KEY_LEFT || k == it->KEY_RIGHT) it->stop();
@@ -53,12 +56,14 @@ bool handle_keys(Players& players) {
 	return true;
 }
 
+/// Double buffering
 void flip() {
 	SDL_GL_SwapBuffers();
 	glClear(GL_COLOR_BUFFER_BIT);
 	glLoadIdentity();
 }
 
+/// OpenGL initialization
 void setup_gl() {
 	glViewport(0, 0, scrW, scrH);
 	glMatrixMode(GL_PROJECTION);
@@ -76,7 +81,7 @@ void setup_gl() {
 	glEnable(GL_BLEND);
 }
 
-
+/// Game loop
 bool main_loop() {
 	TextureMap tm = load_textures();
 	World world(scrW, scrH, tm);
@@ -95,25 +100,51 @@ bool main_loop() {
 	return false;
 }
 
+/// Server runs here
+void server_loop() {
+	TextureMap tm;
+	World world(scrW, scrH, tm);
+	world.addActor(scrW-100, scrH/2, tm.find("tomato")->second);
+	world.addActor(100, scrH/2, tm.find("tomato")->second);
+	Players& players = world.getActors();
 
+	// MAIN LOOP
+	while (true) {
+		world.update();
+		// Compose game-state to send to clients
+		for (Players::const_iterator it = players.begin(); it != players.end(); ++it) {
+			// TODO: compose game state message
+		}
+		// TODO: Send game state to clients
+	}
+}
+
+/// Program entry-point
 int main(int argc, char** argv) {
+	bool dedicated_server = false;
+
+	// TODO: Argument handling
+
 	if (SDL_Init(SDL_INIT_VIDEO|SDL_INIT_JOYSTICK) ==  -1) throw std::runtime_error("SDL_Init failed");
 	//SDL_WM_SetCaption(PACKAGE " " VERSION, PACKAGE);
 
 	SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
 	SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
 
-	SDL_Surface* screen = 0;
+	SDL_Surface* screen = NULL;
 	screen = SDL_SetVideoMode(800, 600, 32, SDL_OPENGL);
 	if (!screen) throw std::runtime_error(std::string("SDL_SetVideoMode failed ") + SDL_GetError());
 	SDL_EnableKeyRepeat(50, 50);
-
-	setup_gl();
 	srand(time(NULL)); // Randomize RNG
 
-	main_loop();
+	// TODO: Main menu
 
-	// TODO: SDL_Quit creates a crash if Texture_2D is enabled in player.draw()
-	//SDL_Quit();
+	if (!dedicated_server) {
+		setup_gl();
+		main_loop();
+		// TODO: SDL_Quit creates a crash if Texture_2D is enabled in player.draw()
+		//SDL_Quit();
+	} else server_loop();
+
 	return 0;
 }
