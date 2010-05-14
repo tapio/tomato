9e3dd6e0077466c2115d2a938cd1e28808548a35
Tapio Vierros <tapiovierros@gmail.com>
2010-05-14 13:51:11 +0300

    Fix publish script origin.

--- tools/publish-winbuild.sh
+++ tools/publish-winbuild.sh
@@ -5,5 +5,5 @@
 
 DATE=`date -R`
 
-make install && git checkout winbuild && cp --recursive tomaatti/* .. && git commit -a -m "Win32 build $DATE" && git push github winbuild && git checkout master
+make install && git checkout winbuild && cp --recursive tomaatti/* .. && git commit -a -m "Win32 build $DATE" && git push origin winbuild && git checkout master
 
