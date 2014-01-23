314e0441eccf035f3426e56a467ecf387824fa24
Tapio Vierros <tapiovierros@gmail.com>
2014-01-23 21:44:36 +0200

    Update deps download links.

--- win32/dependencies.bat
+++ win32/dependencies.bat
@@ -4,17 +4,16 @@ REM setting up the dependencies.
 REM DOWNLOAD
 
 REM Boost http://www.boost.org/
-wget http://downloads.sourceforge.net/project/boost/boost/1.42.0/boost_1_42_0.7z?use_mirror=switch
+wget http://downloads.sourceforge.net/project/boost/boost/1.53.0/boost_1_53_0.7z
 
 REM Box2D http://www.box2d.org/
-wget http://box2d.googlecode.com/files/Box2D_v2.1.2.zip
+wget https://box2d.googlecode.com/files/Box2D_v2.3.0.7z
 
 REM ENet http://enet.bespin.org/
-wget http://enet.bespin.org/download/enet-1.2.1.tar.gz
+http://enet.bespin.org/download/enet-1.3.11.tar.gz
 
 REM SDL http://www.libsdl.org/
-wget http://www.libsdl.org/release/SDL-devel-1.2.14-mingw32.tar.gz
-REM wget http://www.libsdl.org/release/SDL-1.2.14-win32.zip
+wget http://www.libsdl.org/release/SDL2-devel-2.0.1-mingw.tar.gz
 
 REM SOIL http://www.lonesock.net/soil.html
 wget http://www.lonesock.net/files/soil.zip
